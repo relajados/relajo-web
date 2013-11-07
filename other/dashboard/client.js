0a4e78549537209c10b63402c7a84c6c1a952e44
Aythami Santana <aythami.santana@aentos.es>
2013-11-07 14:38:09 +0000

    Created middleman app

--- other/dashboard/client.js
+++
@@ -1,5 +0,0 @@
-var Firebase = require('firebase');
-var myRootRef = new Firebase('https://relajoapp.firebaseio.com');
-myRootRef.on('child_added', function(childSnapshot, prevChildName) {
-  console.log(childSnapshot.val());
-});
